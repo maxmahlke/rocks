2af7f1377704078f51e476f05c96b2f24404f3ec
Max Mahlke <max.mahlke@oca.eu>
2021-02-07 17:07:13 +0100

    Readthedocs fix IV

--- setup.py
+++ setup.py
@@ -2,13 +2,21 @@ import os
 
 from setuptools import find_packages, setup
 
+# ReadTheDocs install does not allow data_files
+if os.environ.get("READTHEDOCS", False):
+    include_package_data = False
+    data_files = []
+else:
+    include_package_data = True
+    data_files = [os.path.join(os.path.expanduser("~"), ".cache/rocks")]
+
 setup(
     name="rocks",
     version="0.1.0",
     python_requires=">=3.4",
-    data_files=[os.path.join(os.path.expanduser("~"), ".cache/rocks")],
     packages=find_packages("rocks"),
-    include_package_data=True,
+    include_package_data=include_package_data,
+    data_files=data_files,
     description="For space rocks.",
     py_modules=["rocks"],
     install_requires=[
