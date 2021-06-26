98765a5b9f650971341edc6c62412124ebab27fe
Max Mahlke <max.mahlke@oca.eu>
2021-06-26 11:29:55 +0200

    Add __version__ attribute

--- rocks/__init__.py
+++ rocks/__init__.py
@@ -1,5 +1,5 @@
 """For space rocks."""
-import json
+from importlib.metadata import version
 import os
 import sys
 
@@ -14,7 +14,9 @@ from . import ssodnet
 from . import resolve
 from . import utils
 
-# Package auxilliary files
+__version__ = version("space-rocks")
+
+# Path to rocks auxilliary files
 PATH_CACHE = os.path.join(os.path.expanduser("~"), ".cache/rocks")
 PATH_TEMPLATE = os.path.join(PATH_CACHE, "ssoCard_template.json")
 PATH_INDEX = os.path.join(PATH_CACHE, "index.pkl")
