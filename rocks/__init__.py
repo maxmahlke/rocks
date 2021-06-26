fe233607ddbe96fd56cf5d210afa427b6db5befc
Max Mahlke <max.mahlke@oca.eu>
2021-06-26 11:30:22 +0200

    Cleaned __init__

--- rocks/__init__.py
+++ rocks/__init__.py
@@ -3,16 +3,10 @@ from importlib.metadata import version
 import os
 import sys
 
-import pandas as pd
-
+# Expose API to user
 from .core import Rock
 from .core import rocks_ as rocks
 from .resolve import identify
-from . import plots
-from . import properties
-from . import ssodnet
-from . import resolve
-from . import utils
 
 __version__ = version("space-rocks")
 
@@ -21,9 +15,10 @@ PATH_CACHE = os.path.join(os.path.expanduser("~"), ".cache/rocks")
 PATH_TEMPLATE = os.path.join(PATH_CACHE, "ssoCard_template.json")
 PATH_INDEX = os.path.join(PATH_CACHE, "index.pkl")
 
+
+# Check for existence of index file and cache directory
 os.makedirs(PATH_CACHE, exist_ok=True)
 
-# Check for existence of index file
 if not os.path.isfile(PATH_INDEX):
     response = input(
         "The index of numbered asteroids is not present on your "
@@ -36,24 +31,3 @@ if not os.path.isfile(PATH_INDEX):
     else:
         print("The index file is required to run rocks. Exiting.")
         sys.exit()
-
-# Read ssoCard template
-# with open(PATH_TEMPLATE, "r") as file_:
-#     TEMPLATE = json.load(file_)
-
-# TEMPLATE_KEYS = pd.json_normalize(TEMPLATE)
-
-# META_MAPPING = dict(
-#     (meta, meta.replace(".uncertainty", ""))
-#     for meta in TEMPLATE_KEYS
-#     if "uncertainty" in meta
-# )
-
-# SHORTCUTS = {
-#     "physical": set(
-#         [attr.split(".")[2] for attr in TEMPLATE_KEYS if "physical" in attr]
-#     ),
-#     "dynamical": set(
-#         [attr.split(".")[2] for attr in TEMPLATE_KEYS if "dynamical" in attr]
-#     ),
-# }
