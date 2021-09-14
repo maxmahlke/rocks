ee4516af41a9ddf810be3bc71bdb6d58ead72486
Max Mahlke <max.mahlke@oca.eu>
2021-09-14 11:07:56 +0200

    version 1.3.1

--- rocks/__init__.py
+++ rocks/__init__.py
@@ -9,7 +9,7 @@ from .core import Rock
 from .core import rocks_ as rocks
 from .resolve import identify
 
-__version__ = "1.3.0"
+__version__ = "1.3.1"
 
 # Path to rocks auxilliary files
 PATH_CACHE = os.path.join(os.path.expanduser("~"), ".cache/rocks")
