8f4b70efc853a4aea2a606f0aa8dd4c5b8ebace0
Max Mahlke <max.mahlke@oca.eu>
2022-02-12 11:14:13 +0100

    Timing for Berthier+ 2022

--- docs/tutorials/rocks_sdss_moc1.py
+++ docs/tutorials/rocks_sdss_moc1.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
-
 """Identify objects in SDSS Moving-Objects Catalogue DR1 with rocks."""
 
 import numpy as np
 import pandas as pd
 import rocks
+import time
 
 # ------
 # Download SDSS MOC1 (6.2MB)
 data = pd.read_fwf(
-    "https://faculty.washington.edu/ivezic/sdssmoc/ADR1.dat.gz",
+    "https://faculty.washington.edu/ivezic/sdssmoc/ADR1.dat",
     colspecs=[(244, 250), (250, 270)],
     names=["numeration", "designation"],
+    encoding="utf-8",
 )
 
 print(f"Number of observations in SDSS MOC1: {len(data)}")
@@ -29,11 +30,19 @@ data.loc[data.numeration == 0, "numeration"] = np.nan
 # Create list of identifiers by merging 'numeration' and 'designation' columns
 ids = data.numeration.fillna(data.designation)
 print("Identifying known objects in catalogue..")
-names_numbers = rocks.identify(ids)
 
-# Add numbers and names to data
-data["name"] = [name_number[0] for name_number in names_numbers]
-data["number"] = [name_number[1] for name_number in names_numbers]
+start = time.time()
+rocks.identify(ids, progress=True)
+print(f"Getting ids took {time.time() - start:.3f}s.")
 
-data.number = data.number.astype("Int64")  # Int64 supports integers and NaN
-print(data.head())
+start = time.time()
+rocks.rocks(ids, progress=True)
+print(f"Getting rocks took {time.time() - start:.3f}s.")
+# names_numbers = rocks.identify(ids)
+
+# # Add numbers and names to data
+# data["name"] = [name_number[0] for name_number in names_numbers]
+# data["number"] = [name_number[1] for name_number in names_numbers]
+
+# data.number = data.number.astype("Int64")  # Int64 supports integers and NaN
+# print(data.head())
