b1106495b21a2946bf3cb1242428d5a71768bd1e
Max Mahlke <max.mahlke@oca.eu>
2022-11-17 18:33:32 +0100

    Not using this piece of SO wisdom

--- rocks/ssodnet.py
+++ rocks/ssodnet.py
@@ -14,19 +14,6 @@ from rocks import config
 from rocks.logging import logger
 
 
-def is_notebook() -> bool:
-    try:
-        shell = get_ipython().__class__.__name__
-        if shell == "ZMQInteractiveShell":
-            return True  # Jupyter notebook or qtconsole
-        elif shell == "TerminalInteractiveShell":
-            return False  # Terminal running IPython
-        else:
-            return False  # Other type (?)
-    except NameError:
-        return False  # Probably standard Python interpreter
-
-
 def get_ssocard(id_ssodnet, progress=False, local=True):
     """Retrieve the ssoCard of one or many asteroids, using their SsODNet IDs.
 
