b3404b3748784a56189a979f17091d4c1b8a1205
Max Mahlke <max.mahlke@oca.eu>
2022-11-26 21:13:45 +0100

    Update index test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,45 +1,17 @@
-#! /usr/bin/env python
-"""Test suit for rocks.utils module.
-"""
-
-import json
-import os
-import pickle
-import warnings
-
-import pandas as pd
-import pickle
-import pytest
-import requests
+#!/usr/bin/env python
+"""Test suit for rocks.utils module. """
 
 import rocks
 
 
-# def test_create_index(monkeypatch):
-#     """Tests index creation routine with cached file and no missing entries."""
-
-#     def read_fwf_monkey(url, colspecs, names):
-
-#         PATH_INDEX = pd.read_csv(
-#             os.path.join(os.path.expanduser("~"), ".cache/rocks/index.pkl")
-#         )
-
-#         with open(PATH_INDEX, "rb") as file_:
-#             index = pickle.load(file_)
-#         print(index)
-
-#         return index[:1000]
-
-#     monkeypatch.setattr(pd, "read_fwf", read_fwf_monkey)
-#     rocks.utils.create_index()
-
-
-def test_read_index():
+def test_index_parts():
     """Test availability and readability of index."""
 
     # Availability
-    assert os.path.isfile(rocks.PATH_INDEX)
+    assert rocks.config.PATH_INDEX.is_dir()
 
     # Readability
-    index = rocks.utils.read_index()
-    assert isinstance(index, pd.DataFrame)
+    for id_ in ["mette", "2012aa14", 230, 12301]:
+        index = rocks.index._get_index_file(id_)
+        assert isinstance(index, dict)
+        assert id_ in index
