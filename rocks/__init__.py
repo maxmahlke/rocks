0dfa0ad97f23abe56b29421d9ac42c1a8ceca3f6
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-03-05 10:23:18 +0100

    version 1.7.4

--- rocks/__init__.py
+++ rocks/__init__.py
@@ -3,12 +3,11 @@
 import inspect
 
 # Welcome to rocks
-__version__ = "1.7.3"
+__version__ = "1.7.4"
 
 # Only define user API if rocks is not called via command line
 context = inspect.stack()[-1].code_context
 if context is None or "rocks.cli" not in context[0]:
-
     # Expose API to user
     from .core import Rock
     from .core import rocks_ as rocks
