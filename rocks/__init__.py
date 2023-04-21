79d19b89bdb2996da963b99a5efbd7fd88b71470
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-04-21 09:50:35 +0200

    version 1.7.5

--- rocks/__init__.py
+++ rocks/__init__.py
@@ -3,7 +3,7 @@
 import inspect
 
 # Welcome to rocks
-__version__ = "1.7.5"
+__version__ = "1.7.6"
 
 # Only define user API if rocks is not called via command line
 context = inspect.stack()[-1].code_context
