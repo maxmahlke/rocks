a4918a38da36cedf3a33767144720d713f6b5ff9
Max Mahlke <max.mahlke@rwth-aachen.de>
2023-08-28 13:07:52 +0200

    Add lite version of bft

--- rocks/bft.py
+++ rocks/bft.py
@@ -5,10 +5,50 @@ from rocks import config
 from rocks import ssodnet
 
 PATH = config.PATH_CACHE / "ssoBFT-latest.parquet"
+PATH_LITE = config.PATH_CACHE / "ssoBFT-latest-lite.parquet"
+
+LITE_COLUMNS = [
+    "sso_id",
+    "sso_number",
+    "sso_name",
+    "sso_class",
+    "orbital_elements.semi_major_axis.value",
+    "orbital_elements.eccentricity.value",
+    "orbital_elements.inclination.value",
+    "orbital_elements.orbital_period.value",
+    "orbital_elements.perihelion_distance.value",
+    "proper_elements.proper_semi_major_axis.value",
+    "proper_elements.proper_eccentricity.value",
+    "proper_elements.proper_inclination.value",
+    "proper_elements.proper_sine_inclination.value",
+    "family.family_number",
+    "family.family_name",
+    "pair.sibling_number",
+    "pair.sibling_name",
+    "pair.distance",
+    "pair.age.value",
+    "yarkovsky.dadt.value",
+    "yarkovsky.A2.value",
+    "yarkovsky.S",
+    "albedo.value",
+    "absolute_magnitude.value",
+    "density.value",
+    "diameter.value",
+    "mass.value",
+    "taxonomy.class",
+    "taxonomy.complex",
+    "thermal_inertia.value",
+    "spins.1.period.value",
+]
 
 
-def load_bft():
+def load_bft(lite=False):
     """Load the BFT from the cache or optionally from remote.
+
+    Parameters
+    ----------
+    lite : bool
+        Return the lite version of the ssoBFT. Default is False.
 
     Returns
     -------
@@ -22,5 +62,15 @@ def load_bft():
         else:
             return None
 
+    if lite:
+        if not PATH_LITE.is_file():
+            _build_lite()
+        return pd.read_parquet(PATH_LITE)
+    return pd.read_parquet(PATH)
+
+
+def _build_lite():
+    """Build the lite version of the ssoBFT."""
     bft = pd.read_parquet(PATH)
-    return bft
+    lite = bft[LITE_COLUMNS]
+    lite.to_parquet(PATH_LITE)
