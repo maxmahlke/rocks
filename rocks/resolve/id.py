14f0371e148921239aa64049dfaa02458e0828a9
Max Mahlke <max.mahlke@rwth-aachen.de>
2024-04-22 14:52:44 +0200

    Add repr for id based on object type

--- rocks/resolve/id.py
+++ rocks/resolve/id.py
@@ -10,3 +10,16 @@ class Id:
     number: int = None
     reduced: str = None
     aliases: [str] = None
+
+    def __bool__(self):
+        return self.name is not None
+
+    def __str__(self):
+        if self.type in ["Asteroid", "Dwarf Planet"]:
+            return f"({self.number}) {self.name}"
+        return f"[{self.name} {self.type}]"
+
+    def __repr__(self):
+        if self.type in ["Asteroid", "Dwarf Planet"]:
+            return f"Id({self.name}, {self.number})"
+        return f"Id(name={self.name})"
